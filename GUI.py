efe76d7004c0c925e9111b29fa78e99b7be836ff
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-03-20 19:22:16 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -277,7 +277,7 @@ def select_mario_folder():
     ratio_value = (int(numerator_entry.get()) / int(denominator_entry.get()))
     scaling_factor = (16/9) / (int(numerator_entry.get()) / int(denominator_entry.get()))
     username = getpass.getuser()
-    gameid = "0100B99019412000"
+    gameid = "010028600EBDA000"
     if output_yuzu.get() is True:
         input_folder = f"C:/Users/{username}/AppData/Roaming/yuzu/load/{gameid}"
         process_name = "yuzu.exe"
